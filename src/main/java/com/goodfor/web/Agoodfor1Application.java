fae7072268a5f19d5c0448e186421df5bb434006
sejong7007 <psj7007@gmail.com>
2020-01-03 12:28:15 +0900

    first commit

---
+++ src/main/java/com/goodfor/web/Agoodfor1Application.java
@@ -0,0 +1,13 @@
+package com.goodfor.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Agoodfor1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Agoodfor1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/goodfor/web/Agoodfor1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodfor.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Agoodfor1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
